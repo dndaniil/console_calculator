e79783de0d496fe3f6804965ddd8e06314fc0902
dndaniil <gedase2005@gmail.com>
2024-04-23 13:53:37 +0300

    calculator

--- console_calculator/main.swift
+++ console_calculator/main.swift
@@ -1,11 +1,36 @@
-//
-//  main.swift
-//  console_calculator
-//
-//  Created by Даниил Гераськин on 23.04.2024.
-//
+print("Добро пожаловать в программу калькулятор")
 
-import Foundation
+print("Выберите операцию: +, -, *  или /")
+let operation = readLine() ?? ""
 
-print("Hello, World!")
+print("Введите первое число:")
+let firstNunber = readLine() ?? ""
+
+print("Введите второе число:")
+let secondNunber = readLine() ?? ""
+
+print("Идет вычисление примера: ", firstNunber + " " + operation + " " + secondNunber)
+
+if let firstNunber = Int(firstNunber) {
+   if let secondNunber = Int(secondNunber) {
+       switch operation {
+       case "+": print("Результат: " + String( firstNunber + secondNunber))
+       case "-": print("Результат: " + String( firstNunber - secondNunber))
+       case "*": print("Результат: " + String( firstNunber * secondNunber))
+       case "/":
+           if secondNunber != 0 {
+               print("Результат: " + String( firstNunber / secondNunber))
+           } else {
+               print("Делить на 0 нельзя")
+           }
+           
+       default: print("Вы ввели неверную операцию")
+       }
+    } else {
+        print("Вы ввели неверное первое число")
+    }
+} else {
+    print("Вы ввели неверное первое число")
+}
+
 
